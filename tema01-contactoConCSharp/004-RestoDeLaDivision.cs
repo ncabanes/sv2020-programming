da3012891edfcc85547bea2707163a5336717a74
Nacho Cabanes <ncabanes@users.noreply.github.com>
2020-09-29 12:39:28 +0200

    Ej.004 - Resto de la división

---
+++ tema01-contactoConCSharp/004-RestoDeLaDivision.cs
@@ -0,0 +1,14 @@
+/* Angel (...)
+   Ejercicio: Calcula el resto de la división de 775 entre 16 */
+
+using System;
+
+class Ejercicio04
+{    
+    static void Main()
+    {
+        
+        Console.WriteLine("El resto de la división es: {0}", 775 % 16);
+    
+    }
+}
