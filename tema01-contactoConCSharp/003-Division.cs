46433291d099631908116c6953e769294db1170f
Nacho Cabanes <ncabanes@users.noreply.github.com>
2020-09-29 12:36:44 +0200

    Ej.003 - División entre enteros

---
+++ tema01-contactoConCSharp/003-Division.cs
@@ -0,0 +1,9 @@
+﻿class Ejercicios_Semana2_3
+{
+    static void Main()
+    {
+        System.Console.WriteLine("El resultado de dividir 11357 entre 24 es");
+        System.Console.WriteLine(11357 / 24);
+    }
+}
+
