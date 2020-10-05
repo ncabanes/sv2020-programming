88d2879e6206ce89ea19881a1c38ae113ed7dd9b
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-05 13:34:35 +0200

    Ej.017 - Número de dia dentro del año

---
+++ tema02-estructurasDeControl/017-DiaEnElAnyo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+//ALBERTO (...)
+//CONTAR DIA EN EL AÑO
+
+class DiaEnElAnyo
+{
+    static void Main()
+    {
+        Console.WriteLine("INTRODUCE EL MES EN NUMERO");
+        int mes = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("INTRODUCE EL DIA EN NÚMERO");
+        int dia = Convert.ToInt32(Console.ReadLine());
+
+        if (mes == 1)//ENERO
+            Console.Write("Es el día número: {0}", dia);
+        else if (mes == 2)//FEBRERO
+            Console.Write("Es el día número: {0}", dia + 31);
+        else if (mes == 3)//MARZO
+            Console.Write("Es el día número: {0}", dia + 31 + 28);
+        else if (mes == 4)//ABRIL
+            Console.Write("Es el día número: {0}", dia + 31 + 28 + 31);
+        else if (mes == 5)//MAYO
+            Console.Write("Es el día número: {0}", dia + 31 + 28 + 31 + 30);
+        else if (mes == 6)//JUNIO
+            Console.Write("Es el día número: {0}",
+                dia + 31 + 28 + 31 + 30 + 31);
+        else if (mes == 7)//JULIO
+            Console.Write("Es el día número: {0}",
+                dia + 31 + 28 + 31 + 30 + 31 + 30);
+        else if (mes == 8)//AGOSTO
+            Console.Write("Es el día número: {0}",
+                dia + 31 + 28 + 31 + 30 + 31 + 30 + 31);
+        else if (mes == 9)//SEPTIEMBRE
+            Console.Write("Es el día número: {0}",
+                dia + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31);
+        else if (mes == 10)//OCTUBRE
+            Console.Write("Es el día número: {0}",
+                dia + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30);
+        else if (mes == 11)//NOVIEMBRE
+            Console.Write("Es el día número: {0}",
+                dia + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31);
+        else if (mes == 12)//DICIEMBRE
+            Console.Write("Es el día número: {0}",
+                dia + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30);
+    }
+
+}
+
