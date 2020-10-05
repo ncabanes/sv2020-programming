a9998d844b98f55ba181fd26f2a213db861b445c
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-05 13:15:24 +0200

    Ej.014 - Menor de dos (2 versiones)

---
+++ tema02-estructurasDeControl/014a-MenorDeDos1.cs
@@ -0,0 +1,28 @@
+﻿/* Alejandro (...)
+ * Devuelve cual es el menor de dos numeros distintos*/
+ 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero1, numero2;
+        Console.WriteLine("Dime el primer número");
+        numero1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Dime el segundo número");
+        numero2 = Convert.ToInt32(Console.ReadLine());
+        if (numero1 == numero2)
+        {
+            Console.WriteLine("Son el mismo número");
+        } 
+        else if (numero1 > numero2)
+        {
+            Console.WriteLine("El segundo número es menor que el primero, {0} es menor que {1}", numero2, numero1);
+        } 
+        else
+        {
+            Console.WriteLine("El primer número es menor que el segundo, {0} es menor que {1}", numero1, numero2);
+        }
+    }
+}
---
+++ tema02-estructurasDeControl/014b-MenorDeDos2.cs
@@ -0,0 +1,25 @@
+﻿// Jorge (...)
+// Ejercicio 14. Pedir dos númerosDecir que número es el menor.
+using System;
+
+class ejercicio14
+{
+    static void Main()
+    {
+        int n1, n2;
+
+        Console.Write("Introduce un número: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Introduce otro número: ");
+        n2 = Convert.ToInt32(Console.ReadLine());
+
+        if (n1 < n2)
+        {
+            Console.WriteLine("{0} es el menor.", n1);
+        }
+        else
+        {
+            Console.WriteLine("{0} es el menor.", n2);
+        }
+    }
+}
