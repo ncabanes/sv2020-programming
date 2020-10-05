677b8ca54c83912b735df476a3c057c9fcc89ad5
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-05 13:19:20 +0200

    Ej.015 - Menor de tres

---
+++ tema02-estructurasDeControl/015-MenorDeTres.cs
@@ -0,0 +1,32 @@
+﻿// Jorge (...)
+// Ejercicio 15. Dice que número es el menor de tres
+
+using System;
+
+class MenorDe3
+{
+    static void Main()
+    {
+        int n1, n2, n3;
+
+        Console.Write("Introduce un número: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Introduce un número: ");
+        n2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Introduce un número: ");
+        n3 = Convert.ToInt32(Console.ReadLine());
+
+        if ((n1 <= n2) && (n1 <= n3))
+        {
+            Console.WriteLine(("{0} es el numero menor."), n1);
+        }
+        else if ((n2 <= n1) && (n2 <= n3))
+        {
+            Console.WriteLine(("{0} es el numero menor."), n2);
+        }
+        else
+        {
+            Console.WriteLine(("{0} es el numero menor."), n3);
+        }
+    }
+}
