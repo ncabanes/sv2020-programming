fcc0e5e0a11ff7074c6a76dac465e6fa803f42d2
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-13 10:37:19 +0200

    Ej.018 - Nombre de un día de la semana

--- tema02-estructurasDeControl/018-DiaDeLaSemana.cs
+++ tema02-estructurasDeControl/018-DiaDeLaSemana.cs
@@ -15,12 +15,12 @@ class Ejercicio_18
         {
             case 1: Console.WriteLine("Lunes"); break;
             case 2: Console.WriteLine("Martes"); break;
-            case 3: Console.WriteLine("Miercole"); break;
+            case 3: Console.WriteLine("Miércoles"); break;
             case 4: Console.WriteLine("Jueves"); break;
             case 5: Console.WriteLine("Viernes"); break;
             case 6: Console.WriteLine("Sábado"); break;
             case 7: Console.WriteLine("Domingo"); break;
-            default: Console.WriteLine("Dái no válido"); break;
+            default: Console.WriteLine("Día no válido"); break;
         }
     }
 }
