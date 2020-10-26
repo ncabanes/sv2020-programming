b51b07816cadf0dc3d727e7295fb5ba8b9a99778
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-26 15:34:01 +0100

    Ej.042 - Triángulo

---
+++ tema02-estructurasDeControl/042-Triangulo.cs
@@ -0,0 +1,21 @@
+﻿//Jorge (...)
+//Triungulo (for)
+
+using System;
+
+class Triangulo
+{
+    static void Main()
+    {
+        Console.Write("Tamaño? ");
+        int numeroAsteriscos = Convert.ToInt32(Console.ReadLine());
+
+        for(int fila = 1; fila <= numeroAsteriscos; fila++)
+        {
+            for (int columna = 1; columna <= fila; columna++)
+                Console.Write("*");
+
+            Console.WriteLine("");
+        }
+    }
+}
