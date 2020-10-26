af5f130a71b3535d22038dab3f92884ada718137
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-26 15:28:28 +0100

    Ej.039 - Asteriscos, con "for"

---
+++ tema02-estructurasDeControl/039-AsteriscosFor.cs
@@ -0,0 +1,24 @@
+﻿/*
+39.Escribe un programa en C# que muestre en pantalla tantos asteriscos como 
+indique el usuario, en una misma fila, usando "for". 
+
+Por ejemplo, si el usuario introduce 7, la respuesta sería: ******* 
+*/
+
+using System;
+
+class Ej39
+{
+    static void Main()
+    {
+        int asteriscos;
+
+        Console.Write("Introduce el número de asteriscos: ");
+        asteriscos = Convert.ToInt32(Console.ReadLine());
+
+        for ( int i = 0; i < asteriscos; i++)
+        {
+            Console.Write("*");
+        }
+    }
+}
