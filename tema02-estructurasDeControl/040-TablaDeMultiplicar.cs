54b37c01061a6d1d8d41fad87f4f46d9eb16c769
Nacho Cabanes <nacho@nachocabanes.com>
2020-10-26 15:32:31 +0100

    Ej.040 - Tablas de multiplicar, con "for"

---
+++ tema02-estructurasDeControl/040-TablaDeMultiplicar.cs
@@ -0,0 +1,23 @@
+﻿/*40.Muestra las tablas de multiplicar de los números del 0 al 10, suando 
+"for". Debe haber una línea en blanco separando cada tabla de multiplicar de la 
+siguiente.*/
+
+using System;
+
+class Ej49
+{
+    static void Main()
+    {
+
+        for ( int tabla = 1; tabla <= 10; tabla++)
+        {
+            Console.WriteLine("TABLA DEL " + tabla );
+            for ( int numero = 0; numero <= 10; numero++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", 
+                    tabla, numero, tabla*numero);
+            }
+            Console.WriteLine();
+        }
+    }
+}
