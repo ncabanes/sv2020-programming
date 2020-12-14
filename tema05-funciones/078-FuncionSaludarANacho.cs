ab07b31998d0f0c602b9ce0f32f041804c053c93
Nacho Cabanes <ncabanes@users.noreply.github.com>
2020-12-14 16:11:52 +0100

    Ej.078 - Función SaludarANacho

---
+++ tema05-funciones/078-FuncionSaludarANacho.cs
@@ -0,0 +1,19 @@
+//78. Crea una función llamada "SaludarANacho", que escriba "Hola, Nacho".
+//Úsala desde Main.
+//ALBERTO (...)
+
+using System;
+
+class Ej078
+{
+    static void SaludarANacho()
+    {
+        Console.Write("Hola, Nacho");
+    }
+    
+    static void Main()
+    {
+        SaludarANacho();   
+    }
+}
+
