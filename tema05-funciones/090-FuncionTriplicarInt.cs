9680abe6fa21e8a5262b78614a5e41fc47f8cb17
Nacho Cabanes <nacho@nachocabanes.com>
2020-12-21 11:41:14 +0100

    Ej.090 - Función Triplicar (int)

---
+++ tema05-funciones/090-FuncionTriplicarInt.cs
@@ -0,0 +1,23 @@
+﻿/*90. Crea una función entera llamada "Triplicar", que devolverá triplicado
+el valor del número entero pasado como parámetro (por ejemplo, si el parámetro
+es 5, se devolverá 15). Pruébala desde Main.*/
+
+//ALBERTO (...)
+
+
+using System;
+
+class EJERCICIO90
+{
+    static void Main()
+    {
+        Console.WriteLine(Triplicar(5));
+    }
+    
+    static int Triplicar(int numero)
+    {
+        int resultado = numero * 3;
+        return resultado;
+    }
+}
+
