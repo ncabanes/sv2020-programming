2ff60ceed4e634dcdd7f87e3da3ec5d1e9d0c30f
Nacho Cabanes <nacho@nachocabanes.com>
2021-01-11 12:24:28 +0100

    Ej.109 - Parámetros por defecto y con nombre

---
+++ tema05-funciones/109-ParamsPorDefecto.cs
@@ -0,0 +1,34 @@
+﻿//Jorge (...)
+//Ejercicio109, Mejora Escribircentrado
+
+using System;
+
+class ejercicio109
+{
+    static void EscribirCentradoYSubrayado(String cadena, char letra='-')
+    {
+        int posicionInicial = 0;
+
+        posicionInicial = (80 - cadena.Length) / 2;
+        for (int i = 0; i < posicionInicial; i++)
+            Console.Write(" ");
+        Console.WriteLine(cadena);
+
+        for (int i = 0; i < posicionInicial; i++)
+            Console.Write(" ");
+        for (int i = 0; i < cadena.Length; i++)
+            Console.Write(letra);
+    }
+    
+    static void Main()
+    {
+        EscribirCentradoYSubrayado("Hola", '=');
+        Console.WriteLine();
+        
+        EscribirCentradoYSubrayado("Hola otra vez");
+        Console.WriteLine();
+        
+        EscribirCentradoYSubrayado(letra: '.', cadena: "Hasta luego");
+        Console.WriteLine();
+    }
+}
