5f1ee6cbfe8a860a9c208f166998159e4f65fef0
Nacho Cabanes <nacho@nachocabanes.com>
2021-01-11 12:19:59 +0100

    Ej.101 - arg, Multiplicar valores en líena de comandos

---
+++ tema05-funciones/101a-MultiplicarArgs.cs
@@ -0,0 +1,28 @@
+﻿/*101. Crea un programa llamado "Multiplicar", que reciba dos números enteros 
+en línea de comandos y muestre su producto, como en este ejemplo:
+
+multiplicar 5 3
+15
+*/
+
+//ALBERTO (...)
+
+using System;
+
+class EJERCICICIO101
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 2)
+        {
+            int primerNumero = Convert.ToInt32(args[0]);
+            int segundoNumero = Convert.ToInt32(args[1]);
+            Console.WriteLine(primerNumero*segundoNumero);
+        }
+        else
+        {
+            Console.WriteLine("Uso: multiplicar 5 3");
+        }
+    }
+}
+
