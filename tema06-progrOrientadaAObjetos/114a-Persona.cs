528f4ecad7a0b07a7b76f0ab458ddfc00a677093
Nacho Cabanes <nacho@nachocabanes.com>
2021-01-18 13:47:44 +0100

    Ej.114 - Clase Persona, VS

---
+++ tema06-progrOrientadaAObjetos/114a-Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Persona
+{
+    private string nombre;
+    private byte edad;
+    private string eMail;
+
+    public string GetNombre() { return nombre; }
+    public byte GetEdad() { return edad; }
+    public string GetEmail() { return eMail; }
+
+    public void SetNombre(string nuevoNombre) { nombre = nuevoNombre; }
+    public void SetEdad(byte nuevaEdad) { edad = nuevaEdad; }
+    public void SetEmail(string nuevoEmail) { eMail = nuevoEmail; }
+
+    public void Mostrar()
+    {
+        Console.WriteLine("Nombre: {0}, edad: {1}, eMail: {2}",
+            nombre, edad, eMail);
+    }
+}
---
+++ tema06-progrOrientadaAObjetos/114b-PruebaPersona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/* Crea una nueva versión del ejercicio 113, en la que cada clase esté en un 
+ * fichero independiente, empleando Visual Studio (o alguna herramienta 
+ * similar). */
+
+// Eduardo (...)
+
+class PruebaPersona
+{
+    static void Main()
+    {
+        Persona p1 = new Persona();
+        Persona p2 = new Persona();
+
+        Console.WriteLine("Nombre de la primera persona:");
+        p1.SetNombre(Console.ReadLine());
+        Console.WriteLine("Edad de la primera persona:");
+        p1.SetEdad(Convert.ToByte(Console.ReadLine()));
+        Console.WriteLine("Email de la primera persona:");
+        p1.SetEmail(Console.ReadLine());
+
+        Console.WriteLine("Nombre de la segunda persona:");
+        p2.SetNombre(Console.ReadLine());
+        Console.WriteLine("Edad de la segunda persona:");
+        p2.SetEdad(Convert.ToByte(Console.ReadLine()));
+        Console.WriteLine("Email de la segunda persona:");
+        p2.SetEmail(Console.ReadLine());
+
+        Console.WriteLine();
+        p1.Mostrar();
+        p2.Mostrar();
+    }
+}
