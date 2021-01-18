46095cdd2d8c07aa1f0dac746277e71432f4c583
Nacho Cabanes <nacho@nachocabanes.com>
2021-01-18 13:46:37 +0100

    Ej.112 - Clase Titulo + Get, Set

---
+++ tema06-progrOrientadaAObjetos/112-TituloGetSet.cs
@@ -0,0 +1,63 @@
+﻿/*112.Crea una nueva versión de la clase "Titulo". Sus atributos serán privados 
+y tendrán getters y setters. Cambia el programa y "Main" según sea necesario.
+Deberás entregar sólo el fichero .cs.*/
+
+//ALBERTO (...)
+
+using System;
+
+class Titulo
+{
+    private int x;
+    private int y;
+    private string texto = "texto en pantalla";
+
+    public void Mostrar()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(texto);
+    }
+
+    public int GetX()
+    {
+        return x;
+    }
+
+    public int GetY()
+    {
+        return y;
+    }
+    
+    public string GetTexto()
+    {
+        return texto;
+    }
+
+    public void SetX(int nuevoValorColumna)
+    {
+        x = nuevoValorColumna;
+    }
+
+    public void SetY(int nuevoValorFila)
+    {
+        y = nuevoValorFila;
+    }
+    
+    public void SetTexto(string nuevoTexto)
+    {
+        texto = nuevoTexto;
+    }
+}
+
+class PruebaDeTitulo
+{
+    static void Main()
+    {
+        Titulo t = new Titulo();
+        t.SetX(30);
+        t.SetY(10);
+        t.SetTexto("Prueba de título");
+        t.Mostrar();
+    }
+}
+
