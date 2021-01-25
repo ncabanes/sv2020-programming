e18167e53cdf247f5ca7cf44f0f8f7a80a04ed4d
Nacho Cabanes <nacho@nachocabanes.com>
2021-01-25 10:53:35 +0100

    Ej.121 - Titulo + constructor

---
+++ tema06-progrOrientadaAObjetos/121-TituloConstructor.cs
@@ -0,0 +1,47 @@
+﻿/*
+
+Crea una nueva versión de la clase "Titulo", usando un constructor en vez del 
+método Inicializar. Pruébalo desde "Main" con los mismo valores que antes.
+
+*/
+
+using System;
+
+class Titulo
+{
+    private int x, y;
+    private string texto;
+
+    public Titulo(int nuevaX, int nuevaY, 
+        string nuevoTexto)
+    {
+        x = nuevaX;
+        y = nuevaY;
+        texto = nuevoTexto;
+    }
+
+    public int GetX() { return x; }
+    public int GetY() { return y; }
+    public string GetTexto() { return texto; }
+
+    public void SetX(int nuevoX) { x = nuevoX; }
+    public void SetY(int nuevoY) { y = nuevoY; }
+    public void SetTexto(string nuevoTexto) { texto = nuevoTexto; }
+
+    public void Mostrar()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(texto);
+    }
+}
+
+
+class PruebaDeTitulo
+{
+    static void Main()
+    {
+        Titulo t = new Titulo(35, 11, "Hola");
+        t.Mostrar();
+    }
+}
+
