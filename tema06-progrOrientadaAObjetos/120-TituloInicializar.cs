dc98b82abb20f980f23450b9d2a5e7dc197c76db
Nacho Cabanes <nacho@nachocabanes.com>
2021-01-25 10:53:11 +0100

    Ej.120 - Titulo + Inicializar

---
+++ tema06-progrOrientadaAObjetos/120-TituloInicializar.cs
@@ -0,0 +1,47 @@
+﻿/*
+Crea un método "Inicializar" para la clase "Título", que permita fijar a la vez 
+los valores iniciales para x, y, texto. Pruébalo desde "Main" con los valores: 
+texto = "Hola", x = 35, y = 11
+*/
+
+using System;
+
+class Titulo
+{
+    private int x, y;
+    private string texto;
+
+    public void Inicializar(int nuevaX, int nuevaY, 
+        string nuevoTexto)
+    {
+        x = nuevaX;
+        y = nuevaY;
+        texto = nuevoTexto;
+    }
+
+    public int GetX() { return x; }
+    public int GetY() { return y; }
+    public string GetTexto() { return texto; }
+
+    public void SetX(int nuevoX) { x = nuevoX; }
+    public void SetY(int nuevoY) { y = nuevoY; }
+    public void SetTexto(string nuevoTexto) { texto = nuevoTexto; }
+
+    public void Mostrar()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(texto);
+    }
+}
+
+
+class PruebaDeTitulo
+{
+    static void Main()
+    {
+        Titulo t = new Titulo();
+        t.Inicializar(35, 11, "Hola");
+        t.Mostrar();
+    }
+}
+
