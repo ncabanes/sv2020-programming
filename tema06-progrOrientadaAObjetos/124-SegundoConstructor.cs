16a7fcd3cc1066c7cf15a46e1b4cc966311d54a7
Nacho Cabanes <nacho@nachocabanes.com>
2021-01-25 10:55:07 +0100

    Ej.124 - Segundo constructor

---
+++ tema06-progrOrientadaAObjetos/124-SegundoConstructor.cs
@@ -0,0 +1,89 @@
+/* Verónica (...)
+ 
+124. Crea un segundo constructor para la clase "Título", que permita escoger 
+el texto, pero prefije la Y a 10, y la X a 40 menos la mitad de la longitud 
+del texto. Crea un "Main" que pruebe ambos constructores.*/
+
+using System;
+
+class Titulo
+{
+    protected string texto;
+    protected int x, y;
+    
+    public Titulo(int nuevaX, int nuevaY, 
+        string nuevoTexto)
+    {
+        x = nuevaX;
+        y = nuevaY;
+        texto = nuevoTexto;
+    }
+    
+    public Titulo(string textoElegido)
+    {
+         x = 40 - textoElegido.Length / 2;
+         y = 10;
+         texto = textoElegido;
+    }
+
+    
+    public Titulo()  // Necesario (por ahora) para poder heredar
+    {
+    }
+    
+    public void Mostrar()
+    {
+        Console.SetCursorPosition(x,y);
+        Console.WriteLine(texto);
+    }
+        
+    ~ Titulo()
+    {
+        Console.WriteLine("Destruyendo texto");
+    }
+    
+}
+
+class TituloSubrayado : Titulo
+{
+    public new void Mostrar()
+    {
+        Console.SetCursorPosition(x,y);
+        Console.WriteLine(texto);
+        Console.SetCursorPosition(x,y + 1);
+        Console.WriteLine(new string ('-',texto.Length));
+    }
+
+}
+
+class TituloCentrado : Titulo
+{
+    public new void Mostrar()
+    {
+        Console.SetCursorPosition(x,y);
+        Console.WriteLine(texto);
+        Console.SetCursorPosition(x,y + 1);
+        Console.WriteLine(new string ('-',texto.Length));
+    }
+    
+        
+    public TituloCentrado(int linea, string input)
+    {
+         x = 80/2;
+         y = linea;
+         texto = input;
+    }
+
+}
+
+class PruebaDeTitulo
+{
+    static void Main()
+    {
+        Titulo t1 = new Titulo(35, 11, "Hola");
+        t1.Mostrar();
+        
+        Titulo t2 = new Titulo("Constructor con título elegido");
+        t2.Mostrar();    
+    }
+}
