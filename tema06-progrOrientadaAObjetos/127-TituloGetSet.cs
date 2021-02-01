7050d1b81fa3eb43cf36afc7b20cc58a055d61d3
Nacho Cabanes <nacho@nachocabanes.com>
2021-02-01 12:05:34 +0100

    Ej.127 - Clase Titulo + propiedades

---
+++ tema06-progrOrientadaAObjetos/127-TituloGetSet.cs
@@ -0,0 +1,82 @@
+﻿/*
+Ejericio 127
+Jorge (...)
+1º DAM Semipresencial 
+
+Crea una nueva versión del ejercicio 121 (título con constructor), partiendo
+de la "solución oficial", pero en la que, en vez de "getters" y "setters" 
+convencionales, emplees "propiedades" en formato largo. Prueba su 
+funcionamiento, cambiando la X y el texto antes de mostrar el título.
+*/
+
+using System;
+
+class Titulo
+{
+    private int x;
+    private int y;
+    private string texto;
+
+    public Titulo(int nuevaX, int nuevaY, string nuevoTexto)
+    {
+        x = nuevaX;
+        y = nuevaY;
+        texto = nuevoTexto;
+    }
+
+    public int X
+    {
+        get
+        {
+            return x;
+        }
+        set
+        {
+            x = value;
+        }
+    }
+
+   // public int X { get; set; }
+
+    public int Y
+    {
+        get
+        {
+            return y;
+        }
+        set
+        {
+            y= value;
+        }
+    }
+
+    public String Texto
+    {
+        get
+        {
+            return texto;
+        }
+        set
+        {
+            texto = value;
+        }
+    }
+  
+    public void Mostrar()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine(texto);
+    }
+}
+
+
+class PruebaDeTitulo
+{
+    static void Main()
+    {
+        Titulo t = new Titulo(35, 11, "Hola");
+        t.X = 1;
+        t.Texto = "Hasta luego";
+        t.Mostrar();
+    }
+}
