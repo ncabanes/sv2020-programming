2a203c05e118d7fdb217c658f49cb5b015e92444
Nacho Cabanes <nacho@nachocabanes.com>
2021-02-07 21:10:31 +0100

    Ej.142 - Encriptador static (2 versiones)

---
+++ tema06-progrOrientadaAObjetos/142a-Encriptador1.cs
@@ -0,0 +1,49 @@
+/* Carlos (...)
+
+142. Crea una clase "Encriptador" para cifrar y descifrar texto.
+Tendrá un método "Encriptar", que recibirá una cadena y devolverá otra
+cadena. Será un método estático, por lo que no necesitaremos crear
+ningún objeto de tipo "Encriptador".  También habrá un método de
+"Descifrar".  En este primer acercamiento, el método de cifrado será
+muy simple: para cifrar sumaremos 1 a cada carácter, de modo que "Hola"
+se convierta en "Ipmb", y para descifrar restaremos 1 a cada carácter.
+Crea una clase PruebaDeEncriptador para probarlo. Un ejemplo de uso
+podría ser  string nuevoTexto = Encriptador.Encriptar ("Hola");*/
+
+using System;
+
+class Encriptador
+{
+    public static string Encriptar ( string texto )
+    {
+        string nuevaCadena = "";
+        for ( int i = 0; i < texto.Length; i++ )
+        {
+            char letraSiguiente = texto[i];
+            letraSiguiente++;
+            nuevaCadena = nuevaCadena + letraSiguiente; 
+        }
+        return nuevaCadena;
+    }
+
+    public static string Descifrar ( string texto )
+    {
+        string nuevaCadena = "";
+        for ( int i = 0; i < texto.Length; i++ )
+        {
+            char letraAnterior = texto[i];
+            letraAnterior--;
+            nuevaCadena = nuevaCadena + letraAnterior; 
+        }
+        return nuevaCadena;
+    }
+}
+
+class PruebaDeEncriptador
+{
+    public static void Main()
+    {
+        Console.WriteLine(Encriptador.Encriptar("Hola"));
+        Console.WriteLine(Encriptador.Descifrar("Ipmb"));
+    }
+}
