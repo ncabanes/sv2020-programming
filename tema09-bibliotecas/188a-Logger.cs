0f3627dff8daec930941386aba9a2f60fa0df730
Nacho Cabanes <nacho@nachocabanes.com>
2021-04-25 11:45:53 +0200

    Ej.188 - Logger (2 formas de mostrar la fecha)

---
+++ tema09-bibliotecas/188a-Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+/*Carlos (...)
+ 
+ 188. Crea un programa "logger", que añadirá a un archivo de texto llamado 
+"log.txt" la fecha y hora actuales, en formato AAAA-MM-DD HH:MM:SS.mmm seguida 
+por el texto introducido por el usuario como parámetros de la línea de comandos.
+Por ejemplo, si el ejecutable se llama "logger.exe" y el usuario escribe "logger
+Esto es una prueba" en la línea de comandos, se debe añadir una línea como:
+
+2021-04-20 17:20:02.023 - Esto es una prueba
+*/
+class Logger
+{
+    static int Main(string[] args)
+    {
+        if (args.Length >= 1)
+        {
+            StreamWriter fichero;
+            string input = "";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                input = input + " " + args[i];
+            }
+
+            DateTime ahora = DateTime.Now;
+            string linea = ahora.Year + "-" + ahora.Month.ToString("00") + "-" 
+                + ahora.Day.ToString("00") + " " + ahora.ToString("T") + "." 
+                + ahora.Millisecond + " -" + input;
+
+            fichero = File.AppendText("log.txt");
+            fichero.WriteLine(linea);
+
+            fichero.Close();
+
+            return 0;
+        }
+        else
+        {
+            Console.WriteLine("Por favor, indica en línea de comandos "+
+                "el texto a guardar en el log");
+            return 1;
+        }
+    }
+}
---
+++ tema09-bibliotecas/188b-Logger2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+/*188. Crea un programa "logger", que añadirá a un archivo de texto llamado
+ * "log.txt" la fecha y hora actuales, en formato AAAA-MM-DD HH:MM:SS.mmm 
+ * seguida por el texto introducido por el usuario como parámetros de la línea
+ * de comandos. Porejemplo, si el ejecutable se llama "logger.exe" y el usuario
+ * escribe "logger Esto es una prueba" en la línea de comandos, se debe añadir 
+ * una línea como: 2021-04-20 17:20:02.023 - Esto es una prueba */
+
+//Iván (...)
+
+class Logger
+{
+    static void Main(string[] args)
+    {
+        DateTime fecha = DateTime.Now;
+
+        if (args.Length > 0)
+        {
+            string texto = "";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                texto += args[i] + " ";
+            }
+
+            texto = texto.TrimEnd();
+            
+            try
+            {
+                StreamWriter fichero = File.AppendText("log.txt");
+                fichero.WriteLine(fecha.ToString("yyyy-MM-dd HH:mm:ss.fff") 
+                    + " - " + texto);
+                fichero.Close();
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+                return;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Faltan parámetros");
+        }
+    }
+}
