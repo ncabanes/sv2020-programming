f91b9a916771877db9420631428baa5abdaa0b14
Nacho Cabanes <nacho@nachocabanes.com>
2021-04-25 11:56:51 +0200

    Ej.194 - Info del sistema

---
+++ tema09-bibliotecas/194-SystemInfo.cs
@@ -0,0 +1,30 @@
+//Alumno: Verónica de la Mata Gironés
+/*194. Muestra información sobre el sistema: versión de Windows, versión de 
+Punto Net, nombre de usuario actual, carpeta de documentos y espacio libre y 
+espacio total en todas las particiones de disco (Busca información sobre 
+"DriveInfo")*/
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+class Ejercicio_194
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Versión de Windows: " + Environment.OSVersion);
+        Console.WriteLine("Versión de .Net: " + Environment.Version);
+        Console.WriteLine("Usuario actual: " + Environment.UserName);
+        Console.WriteLine("Carpeta de documentos: " + 
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            
+        DriveInfo[] discos = DriveInfo.GetDrives();
+        foreach (DriveInfo disco in discos)
+        {
+            Console.WriteLine("Disco: " + disco.Name + ", Espacio libre: " + 
+                 (((disco.AvailableFreeSpace/1024)/1024)/1024) + " GB de " +
+                 (((disco.TotalSize/1024)/1024)/1024) + " GB.");
+        }
+    }
+    
+}
