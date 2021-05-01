c89cb31774ed8266bb21fb987eec6a7d0edd2888
Nacho Cabanes <nacho@nachocabanes.com>
2021-05-01 19:27:24 +0200

    Ej.197c - MiniFreddy, enemigo y colisiones

--- tema09-bibliotecas/MiniFreddy/MiniFreddy/Game1.cs
+++ tema09-bibliotecas/MiniFreddy/MiniFreddy/Game1.cs
@@ -13,6 +13,10 @@ namespace MiniFreddy
         private Vector2 posicionPersonaje;
         private int velocidadPersonaje = 200;
 
+        private Texture2D enemigo; 
+        private Vector2 posicionEnemigo; 
+        private Vector2 velocidadEnemigo;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -26,15 +30,18 @@ namespace MiniFreddy
 
         protected override void Initialize()
         {
-            posicionPersonaje = new Vector2(400, 500);
-
             base.Initialize();
+
+            posicionPersonaje = new Vector2(400, 500);
+            posicionEnemigo = new Vector2(300, 100);
+            velocidadEnemigo = new Vector2(150, 100);
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             personaje = Content.Load<Texture2D>("personaje");
+            enemigo = Content.Load<Texture2D>("enemigo");
         }
 
         protected override void Update(GameTime gameTime)
@@ -43,6 +50,7 @@ namespace MiniFreddy
                     || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            // Movimiento del personaje, con teclado
             var estadoTeclado = Keyboard.GetState();
 
             if (estadoTeclado.IsKeyDown(Keys.Left)) 
@@ -61,6 +69,25 @@ namespace MiniFreddy
                 posicionPersonaje.Y += velocidadPersonaje *
                     (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            // Movimiento automático del enemigo
+            posicionEnemigo.X += velocidadEnemigo.X * 
+                (float)gameTime.ElapsedGameTime.TotalSeconds; 
+            posicionEnemigo.Y += velocidadEnemigo.Y * 
+                (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Rebote en los bordes. TO DO: Evitar "números mágicos"
+            if ((posicionEnemigo.X < 20) || (posicionEnemigo.X > 1200)) 
+                velocidadEnemigo.X = -velocidadEnemigo.X; 
+            if ((posicionEnemigo.Y < 20) || (posicionEnemigo.Y > 700))
+                velocidadEnemigo.Y = -velocidadEnemigo.Y;
+
+            // Comprobación de colisiones
+            if (new Rectangle((int)posicionPersonaje.X, (int)posicionPersonaje.Y, 
+                personaje.Width, personaje.Height).Intersects(
+                    new Rectangle((int)posicionEnemigo.X, (int)posicionEnemigo.Y, 
+                    enemigo.Width, enemigo.Height))) 
+                Exit();
+
             base.Update(gameTime);
         }
 
@@ -68,7 +95,10 @@ namespace MiniFreddy
         {
             GraphicsDevice.Clear(new Color(32, 32, 32));
 
-            _spriteBatch.Begin(); 
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(enemigo, 
+                new Rectangle((int)posicionEnemigo.X, (int)posicionEnemigo.Y, 
+                    enemigo.Width, enemigo.Height), Color.White);
             _spriteBatch.Draw(personaje,
                 new Rectangle((int)posicionPersonaje.X, (int)posicionPersonaje.Y, 
                     personaje.Width, personaje.Height),
